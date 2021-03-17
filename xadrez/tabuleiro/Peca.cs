112edcbc018bb5fb1e766dbb9f168f7a9a193c26
Bruno Salgado <bruno_dosreis@outlook.com>
2021-03-17 15:49:28 -0300

    classe posição e peças

---
+++ xadrez/tabuleiro/Peca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+
+    }
+}
